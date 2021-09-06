b415325e6de9c65959a2cb2bba9b7820904679f0
Pavel Žák <pavel@zak.global>
2021-09-06 23:56:10 +0200

    Add cookies message

--- all_my_saints/urls.py
+++ all_my_saints/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from compro import views
 
@@ -23,3 +25,7 @@ urlpatterns = [
     path('', views.home),
     path('compro/<slug:compro_slug>', views.compro, name='compro_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL,
+                                       document_root=settings.STATIC_ROOT)
